385e97474219f363f38186d80e6b181db840a081
AnthonyRQC <88355669+AnthonyRQC@users.noreply.github.com>
2025-07-22 19:58:44 -0400

    Update settings.py

--- centros_ayuda/settings.py
+++ centros_ayuda/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-ba&32i594)6z3miw35-%(ig@jj#a=12!&c0-yi%l76rw3^a39y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
